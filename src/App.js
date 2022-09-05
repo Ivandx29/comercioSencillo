194533c5302fb792e73d7c9096837ba5b8d990f0
Ivan Cruz Martinez <ivandx1@MacBook-Pro-de-Ivan.local>
2022-09-05 13:34:25 -0500

    my first app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Principal from './components/Principal';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Principal />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Principal.js
@@ -0,0 +1,12 @@
+import React, { useState }from "react";
+
+
+const Principal = () => {
+    return (
+        <div className="heeader">
+        <h1>Inicio de mi Pequeño Comercio</h1>
+        
+        </div>
+    );
+}
+export default Principal;
